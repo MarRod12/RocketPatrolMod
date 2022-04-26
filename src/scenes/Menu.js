865b14f379141edbb3e5704f6cde936763bfe1ca
Denae F <102620918+DenaeF@users.noreply.github.com>
2022-04-26 09:09:44 -0700

    Spaceship Speeds faster

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -52,7 +52,7 @@ class Menu extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
           // Novice mode
           game.settings = {
-            spaceshipSpeed: 3,
+            spaceshipSpeed: 5,
             gameTimer: 60000    
           }
           this.sound.play('sfx_select');
@@ -61,7 +61,7 @@ class Menu extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
           // Expert mode
           game.settings = {
-            spaceshipSpeed: 4,
+            spaceshipSpeed: 8,
             gameTimer: 45000    
           }
           this.sound.play('sfx_select');
